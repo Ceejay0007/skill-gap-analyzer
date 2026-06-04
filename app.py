0bfbb5063d6a228b948160d6ee029212d7cdcb77
Conrad Toke <conradtoke@icloud.com>
2026-06-04 03:09:15 -0500

    Added resume parser feature and test function

--- app.py
+++ app.py
@@ -4,6 +4,8 @@
 from src.data.job_ingestion import load_job_description
 from src.nlp.skill_extraction import extract_skills
 from src.data.job_api import fetch_jobs
+from src.data.resume_parser import load_resume
+
 
 """# test for loading a local job file + extracting skills
 def test_local_job_file():
@@ -13,11 +15,21 @@ def test_local_job_file():
 """
 # test for checking if the job API works
 def test_job_api():
-    jobs = fetch_jobs("data analyst", "Houston")
+    jobs = fetch_jobs("data analyst", "USA")
     print("jobs from API:", jobs)
 
+# Test forthe Resume parser feature 
+def test_resume_parser():
+    text = load_resume("CONRAD TOKE_Data Science Intern_20260508.pdf")
+    print("resume text:", text[:500])  # print first 500 chars
+
+    skills = extract_skills(text)
+    print("skills from resume:", skills)
 
 # choose which test to run
 if __name__ == "__main__":
     # test_local_job_file()   # old test, keeping it here for later
-    test_job_api()            # running the API test for now
+    # test_job_api()            # running the API test for now
+    test_resume_parser()
+
+
